9198e3e145eece61677f4a11be54ba8d5f40631f
legen92 <confessional111@gmail.com>
2022-07-13 15:43:36 +0700

    update

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import Slider from "../../components/Slider/Slider";
+import Apple from "../../components/Apple/Apple";
+import style from './Home.module.css'
+import CategoryLaptop from "../../components/Category/Category";
+
 
 function Home() {
   return (
-    <>
+    <div className={style.container}>
       <Slider />
-    </>
+      <CategoryLaptop />
+      <Apple />
+
+    </div>
   );
 }
 
